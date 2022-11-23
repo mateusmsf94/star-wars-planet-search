c680b88f2642aaf1f67c07a795147b69f91c9403
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2022-11-23 14:11:57 -0300

    lint fix 2

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import { AppContext } from '../context/AppProvider';
 import Planet from './Planet';
 
