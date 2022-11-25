2fa66e7392b6b9e0af6172c25502bee3d62aa224
Mateus M. S. A. Figueiredo <mateusmelosf@gmail.com>
2022-11-25 11:14:08 -0300

    requisito 2

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,49 +1,50 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppProvider';
 import Planet from './Planet';
 
 function Table() {
   const { results } = useContext(AppContext);
-  // useEffect(() => {
-  //   // declare the async data fetching function
-  //   const fetchData = async () => {
-  //     // get the data from the api
-  //     const data = await fetch('https://swapi.dev/api/planets');
-  //     // convert data to json
-  //     const json = await data.json();
-  //     json.results.forEach((planet) => {
-  //       delete planet.residents;
-  //     });
-
-  //   };
-
-  //   fetchData();
-  // }, []);
+  const [query, setQuery] = useState('');
+  console.log(results);
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Rotation Period</th>
-          <th>Orbital Period</th>
-          <th>Diameter</th>
-          <th>Climate</th>
-          <th>Gravity</th>
-          <th>Terrain</th>
-          <th>Surface Water</th>
-          <th>Population</th>
-          <th>Films</th>
-          <th>Created</th>
-          <th>Edited</th>
-          <th>URL</th>
-        </tr>
-      </thead>
-      <tbody>
-        {results
-          && results.map((planet, index) => <Planet key={ index } info={ planet } />)}
-      </tbody>
-    </table>
+    <div>
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={ (e) => {
+          setQuery(e.target.value);
+        } }
+        value={ query }
+        data-testid="name-filter"
+      />
+
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {results
+            && results
+              .filter((planet) => planet.name.includes(query))
+              .map((planet, index) => <Planet key={ index } info={ planet } />)}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
